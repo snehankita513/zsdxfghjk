3fb6e10fb1eaaf9ae098f1386da1dbcbb9d1e356
snehankita513 <76689701+snehankita513@users.noreply.github.com>
2021-07-20 16:38:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas')
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results)
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    
+  }
+}
+
+noseX = "";
+noseY = "";
+Gamestatus = "";
+
+function startGame()
+{
+	Gamestatus = "start";
+	document.getElementById("status").innerHTML = "Game is Loading"
+}
+
+function game() {
+	console.log("noseX = " + noseX +", noseY = " + noseY);
+    
+}
+
+
+
+
+
+
